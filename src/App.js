a154609672fa2341b56e6213ad99d82d4737f6d6
lien-eckang <euncho.kang@lien.kr>
2022-07-26 14:06:36 +0900

    Add authentication

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { withAuthenticator, Button, Heading } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { signInButtonContent } from "aws-amplify";
 
-function App({ signInButtonContent, user }) {
-  //Todo logic here
+/* src/App.js */
+function App({ signOut, user }) {
+  // Todo logic here
 
   return (
     <>
+      {/* Add Todo JSX here  */}
       <Heading level={1}>Hello {user.username}</Heading>
       <Button onClick={signOut}>Sign out</Button>
     </>
   );
 }
+
 export default withAuthenticator(App);
